7339ab6f6e7b94f067eccb0f58f99e33fa699bbe
multicare-dataset <multicaredataset@gmail.com>
2024-12-10 16:46:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -310,6 +310,13 @@ def main():
                 for index in range(start_idx, end_idx):
                     display_case_both(cch, index)
             
+            
+            # Procesar la acción de navegación
+            if next_pressed and st.session_state.page_number < total_pages:
+                st.session_state.page_number += 1
+            elif prev_pressed and st.session_state.page_number > 1:
+                st.session_state.page_number -= 1
+
             # Controles de paginación en la parte inferior
             col1_bot, col2_bot, col3_bot = st.columns([1, 2, 1])
             with col1_bot:
@@ -318,12 +325,6 @@ def main():
             with col3_bot:
                 if st.button("Next", key="next_bot"):
                     next_pressed = True
-            
-            # Procesar la acción de navegación
-            if next_pressed and st.session_state.page_number < total_pages:
-                st.session_state.page_number += 1
-            elif prev_pressed and st.session_state.page_number > 1:
-                st.session_state.page_number -= 1
             
             # Mostrar los resultados de la página actual
             page_number = st.session_state.page_number
