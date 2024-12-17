982e4888775e783bd7d0446f740cee7b07fa375c
multicare-dataset <multicaredataset@gmail.com>
2024-12-17 17:55:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -219,8 +219,8 @@ st.markdown(
     }
 
     div[data-testid="stExpanderDetails"] {
-        padding-right: 2rem;
-        padding-left: 1rem;
+        padding-right: 1rem;
+        padding-left: 0rem;
         padding-top: 0rem;
     }
 
@@ -427,14 +427,15 @@ def display_case_text(cch, index):
     article_title = cch.metadata_df[cch.metadata_df.article_id == article_id].title.iloc[0]
 
     with st.container(border=True):
-        st.subheader("Title Here")
-        col1, col2, col3 = st.columns([1, 1, 1])
-        with col1:
-            st.write(f"Case ID: **{case_id}**")
-        with col2:  
-            st.write(f"Gender: **{patient_gender}**")
-        with col3:  
-            st.write(f"Age: **{patient_age}**")
+        with st.container(border=True):
+            st.subheader("Title Here")
+            col1, col2, col3 = st.columns([1, 1, 1])
+            with col1:
+                st.write(f"Case ID: **{case_id}**")
+            with col2:  
+                st.write(f"Gender: **{patient_gender}**")
+            with col3:  
+                st.write(f"Age: **{patient_age}**")
         
         max_characters = 250
         case_text_aux = case_text[:max_characters]  
@@ -443,14 +444,14 @@ def display_case_text(cch, index):
             case_text_aux += case_text[max_characters:max_characters + match.start() + 1]
 
         rest_text = case_text[len(case_text_aux):]
-
-        st.subheader("Case Description")
-        with st.expander(f"{case_text_aux}"):
-            st.markdown(
-                f"<div style='text-align: justify; padding-right: 2rem; padding-left: 3rem; padding-bottom: 2rem;'>{rest_text}</div>",
-                unsafe_allow_html=True
-            )
-        st.write(f"**Citation**: *{article_citation}*")
+        with st.container(border=True):
+            st.subheader("Case Description")
+            with st.expander(f"{case_text_aux}"):
+                st.markdown(
+                    f"<div style='text-align: justify; padding-right: 2rem; padding-left: 3rem; padding-bottom: 2rem;'>{rest_text}</div>",
+                    unsafe_allow_html=True
+                )
+            st.write(f"**Citation**: *{article_citation}*")
 
 
 def display_image(cch, index):
